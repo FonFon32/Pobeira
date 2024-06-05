8b49681831b0a3dc660ed1ad0cb95d005e89fcb3
Afonso <alfixenchannel@gmail.com>
2024-06-05 16:05:20 +0100

    correção

---
+++ server.js
@@ -0,0 +1,67 @@
+let express = require('express')
+let mongoose = require('mongoose')
+let port = 3000
+let app = express()
+
+app.use(express.json())
+app.use(express.static('pages'));
+
+const uri = 'mongodb+srv://40230383:AspCC5GsyPvykzWi@cluster0.e8qcqeu.mongodb.net/Pobeira?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(uri)
+    .then(function() {
+        console.log('Connected to MongooseDB');
+    }) 
+    .catch(function(err) {
+        console.log('Error connecting to MongooseDB', err);
+    })
+
+//modelo e schema ////////////////////////////////////////////////////////////////////
+
+const pobeiraSchema = new mongoose.Schema({
+    alcool: String,
+    name: String,
+    corpo: String,
+    estilo: String,
+    corpo2: String,
+    headliner: String,
+    ebc: Number,
+    ibu: Number
+}, { versionKey: false });
+const Pobeira = mongoose.model('Pobeira', pobeiraSchema);
+
+////inicio do routing /////////////////////////////////////////////////////////////
+
+app.get('/pobeiras', function(req, res) {
+    let query = {};
+    if (req.query.name) {
+        query.name = req.query.name; 
+    }
+
+    Pobeira.find(query) 
+    .then(function(pobeira) {
+        res.send(pobeira)
+    })
+    .catch(function (err) {
+        res.status(500).send({ message: 'Error fetching pobeiras', error: err })
+    })
+});
+
+app.post('/pobeiras', function (req, res) {
+    let newPobeira = new Pobeira(req.body)
+
+    newPobeira
+    .save()
+    .then(function (pobeira) {
+        res.send(pobeira)
+    })
+    .catch(function (err) {
+        res.status(400).send({ message: 'Error adding pobeira', error: err })
+    })
+});
+
+
+////app listen /////////////////////////////////////
+
+app.listen(port, function() {
+    console.log('Connected to http://', port)
+});
