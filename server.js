b42b815fdc407d9eb6266cd34c1c014a555a8613
Miguel <wehdorn21@gmail.com>
2024-06-23 13:16:29 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,67 +1,25 @@
-let express = require('express')
-let mongoose = require('mongoose')
-let port = 3000
-let app = express()
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
 
-app.use(express.json())
-app.use(express.static('public'));
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
 
-const uri = 'mongodb+srv://40230383:AspCC5GsyPvykzWi@cluster0.e8qcqeu.mongodb.net/Pobeira?retryWrites=true&w=majority&appName=Cluster0'
-mongoose.connect(uri)
-    .then(function() {
-        console.log('Connected to MongooseDB');
-    }) 
-    .catch(function(err) {
-        console.log('Error connecting to MongooseDB', err);
-    })
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
 
-//modelo e schema ////////////////////////////////////////////////////////////////////
-
-const pobeiraSchema = new mongoose.Schema({
-    alcool: String,
-    name: String,
-    corpo: String,
-    estilo: String,
-    corpo2: String,
-    headliner: String,
-    ebc: Number,
-    ibu: Number
-}, { versionKey: false });
-const Pobeira = mongoose.model('Pobeira', pobeiraSchema);
-
-////inicio do routing /////////////////////////////////////////////////////////////
-
-app.get('/pobeiras', function(req, res) {
-    let query = {};
-    if (req.query.name) {
-        query.name = req.query.name; 
-    }
-
-    Pobeira.find(query) 
-    .then(function(pobeira) {
-        res.send(pobeira)
-    })
-    .catch(function (err) {
-        res.status(500).send({ message: 'Error fetching pobeiras', error: err })
-    })
-});
-
-app.post('/pobeiras', function (req, res) {
-    let newPobeira = new Pobeira(req.body)
-
-    newPobeira
-    .save()
-    .then(function (pobeira) {
-        res.send(pobeira)
-    })
-    .catch(function (err) {
-        res.status(400).send({ message: 'Error adding pobeira', error: err })
-    })
-});
-
-
-////app listen /////////////////////////////////////
-
-app.listen(port, function() {
-    console.log('Connected to http://localhost/', port)
-});
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
